c737e6501ed5ef426b2410c2b39ed897d5e7c723
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-07 21:33:14 -0400

    Car value implemented.

--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useAppSelector } from '../hooks/store/useAppSelector.ts';
 
 type CarValueProps = {};
 
 const CarValue: React.FC<CarValueProps> = () => {
+
+  const items = useAppSelector(state => state.cars.items);
+
+  const total = useMemo(() => {
+    return items.reduce((acc, currentValue) => {
+      return acc + currentValue.value
+    }, 0);
+  }, [items]);
+
   return (
-    <h1>CarValue</h1>
+    <h1>Total value: {total}</h1>
   );
 };
 
